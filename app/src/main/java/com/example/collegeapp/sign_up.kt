59695bfabfd8f6062f876220aa53f8fefe2d894b
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2023-03-19 15:19:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/collegeapp/sign_up.kt
@@ -0,0 +1,54 @@
+package com.example.collegeapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class sign_up : AppCompatActivity() {
+
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            signUp(email, password)
+        }
+
+
+    }
+
+    private fun signUp(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@sign_up, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@sign_up, "Some error occurred", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
